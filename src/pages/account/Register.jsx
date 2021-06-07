6c62bbbdd94e11937211616314bdd7c8da28792f
redxzeta <xzetanzero@gmail.com>
2021-06-06 22:30:17 -0700

    fixing

--- src/pages/account/Register.jsx
+++ src/pages/account/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
               onChange={updateForm}
             />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              We will never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
