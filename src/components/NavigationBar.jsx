75f184ab031c618b13f089750d370e26d1ebf2cf
redxzeta <xzetanzero@gmail.com>
2021-09-06 12:55:34 -0700

    updating to use bootstrap 5

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,47 +1,49 @@
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
     <Navbar bg="primary" expand="lg">
-      <Navbar.Brand as={Link} to="/">
-        React-Bootstrap
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/test">
-            Test
-          </Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/cards">
-              Cards
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/cards">
-              Another Link
-            </NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/cards">
-              Change Link Here
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item as={Link} to="/cards">
-              Divider Link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/test">
+              Test
+            </Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/cards">
+                Cards
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/cards">
+                Another Link
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/cards">
+                Change Link Here
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/cards">
+                Divider Link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
 
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/cards">
-            Cards
-          </Nav.Link>
-          <Nav.Link as={Link} to="/register">
-            Register
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+          <Nav>
+            <Nav.Link as={Link} to="/cards">
+              Cards
+            </Nav.Link>
+            <Nav.Link as={Link} to="/register">
+              Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
--- src/pages/account/Register.jsx
+++ src/pages/account/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Form, Jumbotron } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import "./register.css";
 const Register = () => {
   const [form, setForm] = useState({ email: "", password: "" });
@@ -17,39 +17,38 @@ const Register = () => {
   };
   return (
     <div className="register__page">
-      <Jumbotron>
-        <h1>REGISTER</h1>
-        <Form onSubmit={onSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              value={form.email}
-              onChange={updateForm}
-            />
-            <Form.Text className="text-muted">
-              We will never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      <h1>REGISTER</h1>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={form.password}
-              onChange={updateForm}
-            />
-          </Form.Group>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={form.email}
+            onChange={updateForm}
+          />
+          <Form.Text className="text-muted">
+            We will never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Jumbotron>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={form.password}
+            onChange={updateForm}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
       {result && <ShowUserInfo>{result}</ShowUserInfo>}
     </div>
   );
